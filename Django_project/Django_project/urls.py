ae2c5399a13a79b1a8b0db078c5320a32336be6e
Md Fahad <fahadbdctglh8@gmail.com>
2023-02-08 15:09:19 +0600

    Creating URL Pattern of Multiple Apps Using Include Function

--- Django_project/Django_project/urls.py
+++ Django_project/Django_project/urls.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-
-from Home import views
-from Blog import views as b
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.Home),
-    path('blog/', b.Blog)
+    path('', include('Home.urls')),
+    path('blog/', include('Blog.urls'))
 ]
