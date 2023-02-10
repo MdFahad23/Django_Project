912f04ddf22006f1ac3f8b720fe08ec338c2399b
Md Fahad <fahadbdctglh8@gmail.com>
2023-02-11 00:33:03 +0600

    Templates and Static Files

--- Django_project/Django_project/settings.py
+++ Django_project/Django_project/settings.py
@@ -16,6 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'Templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 
 
@@ -121,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    STATIC_DIR
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- Django_project/Home/views.py
+++ Django_project/Home/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def Home(request):
     dictatory = {'text_1': 'Home Page is Defined!'}
-    return render(request, 'Home\index.html', context=dictatory)
+    return render(request, 'Home/index.html', context=dictatory)
