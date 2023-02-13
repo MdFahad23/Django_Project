67c346f5edb22c3b3641608861567cdfa77fb478
Md Fahad <fahadbdctglh8@gmail.com>
2023-02-13 20:55:04 +0600

    Createde Models

--- Django_project/Home/models.py
+++ Django_project/Home/models.py
@@ -3,14 +3,27 @@ from django.db import models
 # Create your models here.
 class Musician(models.Model):
     # id = models.AutoField(primary_key=True)
-    first_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50, blank=True)
     last_name = models.CharField(max_length=50)
     instrument = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name + " " + self.instrument
 
 class Album(models.Model):
     artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     release_date = models.DateField()
-    num_start = models.IntegerField()
+    rating = (
+        (1, 'Worst'),
+        (2, 'Bad'),
+        (3, 'Not Bad'),
+        (4, 'Good'),
+        (5, 'Excellent')
+    )
+    num_start = models.IntegerField(choices=rating)
+
+    def __str__(self):
+        return self.name + ", " + str(self.release_date) + ", Rating: " + str(self.num_start)
 
 
