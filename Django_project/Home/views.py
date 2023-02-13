8a1bb6fb07fef29912d2a3c6e2b207fdb0557c95
Md Fahad <fahadbdctglh8@gmail.com>
2023-02-13 21:31:37 +0600

    Views Templates Models sho Data

--- Django_project/Home/views.py
+++ Django_project/Home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from Home.models import Musician, Album
 
 # Create your views here.
 def Home(request):
-    dictatory = {'text_1': 'Home Page is Defined!'}
-    return render(request, 'Home/index.html', context=dictatory)
+    musician_list = Musician.objects.order_by('first_name')
+    diction = {'text_1': 'This is a Musician List', 'musician': musician_list}
+    return render(request, 'Home/index.html', context=diction)
