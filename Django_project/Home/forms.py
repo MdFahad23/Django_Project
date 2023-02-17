3cf7e69b6e94bde1b76ccd60b8ab70c04a3535ac
Md Fahad <fahadbdctglh8@gmail.com>
2023-02-17 16:27:17 +0600

    Django Form Field|widget|validators

---
+++ Django_project/Home/forms.py
@@ -0,0 +1,46 @@
+from django import forms
+from django.core import validators
+
+# def even_or_not(args):
+#     if args%2 == 1:
+#         raise forms.ValidationError('Please Insert or Event Number!')
+    
+
+class User_form(forms.Form):
+    # User_name = forms.CharField(label='User Name', widget=forms.TextInput(attrs={'placeholder':'Enter your full name.', 'style': 'width: 300px'}), validators=[validators.MaxLengthValidator(10), validators.MinLengthValidator(5)])
+
+    # User_dob = forms.DateField(label='Date of Birth', widget=forms.TextInput(attrs={'type': 'date'}))
+
+    # User_email = forms.EmailField(label='User Email', widget=forms.TextInput(attrs={'placeholder': 'Enter your email.'}))
+
+    # field = forms.BooleanField(required=False)
+
+    # field = forms.CharField(max_length=10, min_length=5)
+
+    # choice = (('', '--Select Option--'), ('1', 'First'), ('2', 'Second'), ('3', 'Third'))
+    # field = forms.ChoiceField(choices=choice, required=False )
+
+    # choice = (('A', 'A'), ('B', 'B'), ('C', 'C'))
+    # field = forms.ChoiceField(choices=choice, widget=forms.RadioSelect)
+
+    # choice = (('', '--Select Option--'), ('1', 'First'), ('2', 'Second'), ('3', 'Third'))
+    # field = forms.MultipleChoiceField(choices=choice, required=False )
+
+    # choice = (('A', 'A'), ('B', 'B'), ('C', 'C'))
+    # field = forms.MultipleChoiceField(choices=choice, widget=forms.CheckboxSelectMultiple)
+
+    # field = forms.IntegerField(validators=[even_or_not])
+
+    user_email = forms.EmailField()
+    user_vEmail = forms.EmailField()
+
+    def clean(self):
+        clean_data = super().clean()
+        user_email = clean_data['user_email']
+        user_vEmail = clean_data['user_vEmail']
+
+        if user_email != user_vEmail:
+            raise forms.ValidationError("Email Don't Match!!!")
+        
+
+
--- Django_project/Home/views.py
+++ Django_project/Home/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
 from Home.models import Musician, Album
+from Home import forms
 
 # Create your views here.
 def Home(request):
     musician_list = Musician.objects.order_by('first_name')
     diction = {'text_1': 'This is a Musician List', 'musician': musician_list}
     return render(request, 'Home/index.html', context=diction)
+
+def Form(request):
+    new_form = forms.User_form()
+    diction = {'heading': "This is Create for Django Form.", 'new_form': new_form}
+
+    if request.method == 'POST':
+        new_form = forms.User_form(request.POST)
+        diction.update({'new_form': new_form})
+
+        if new_form.is_valid():
+
+            # user_name = new_form.cleaned_data['User_name']
+            # user_dob = new_form.cleaned_data['User_dob']
+            # user_email = new_form.cleaned_data['User_email']
+    
+            # diction.update({'User_name': user_name})
+            # diction.update({'User_dob': user_dob})
+            # diction.update({'User_email': user_email})
+            # diction.update({'field': new_form.cleaned_data['field']})
+            diction.update({'field': "SuccessFull!"})
+            diction.update({'Submitted': 'True'})
+
+    return render(request, "Home/Form.html", context=diction)
+    
