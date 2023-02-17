cb9e5c8442147feedc9d1f94edf68c769486e4f8
Md Fahad <fahadbdctglh8@gmail.com>
2023-02-17 22:06:59 +0600

    Model for Save Form

--- Django_project/Home/forms.py
+++ Django_project/Home/forms.py
@@ -1,12 +1,13 @@
 from django import forms
 from django.core import validators
+from Home.models import Musician, Album
 
 # def even_or_not(args):
 #     if args%2 == 1:
 #         raise forms.ValidationError('Please Insert or Event Number!')
     
 
-class User_form(forms.Form):
+# class User_form(forms.Form):
     # User_name = forms.CharField(label='User Name', widget=forms.TextInput(attrs={'placeholder':'Enter your full name.', 'style': 'width: 300px'}), validators=[validators.MaxLengthValidator(10), validators.MinLengthValidator(5)])
 
     # User_dob = forms.DateField(label='Date of Birth', widget=forms.TextInput(attrs={'type': 'date'}))
@@ -31,16 +32,23 @@ class User_form(forms.Form):
 
     # field = forms.IntegerField(validators=[even_or_not])
 
-    user_email = forms.EmailField()
-    user_vEmail = forms.EmailField()
+    # user_email = forms.EmailField()
+    # user_vEmail = forms.EmailField()
 
-    def clean(self):
-        clean_data = super().clean()
-        user_email = clean_data['user_email']
-        user_vEmail = clean_data['user_vEmail']
+    # def clean(self):
+    #     clean_data = super().clean()
+    #     user_email = clean_data['user_email']
+    #     user_vEmail = clean_data['user_vEmail']
 
-        if user_email != user_vEmail:
-            raise forms.ValidationError("Email Don't Match!!!")
-        
+    #     if user_email != user_vEmail:
+    #         raise forms.ValidationError("Email Don't Match!!!")
+
+
+
+class Musician(forms.ModelForm):
+    class Meta:
+        model = Musician
+        fields = '__all__'
+            
 
 
--- Django_project/Home/views.py
+++ Django_project/Home/views.py
@@ -9,14 +9,17 @@ def Home(request):
     return render(request, 'Home/index.html', context=diction)
 
 def Form(request):
-    new_form = forms.User_form()
-    diction = {'heading': "This is Create for Django Form.", 'new_form': new_form}
+    # new_form = forms.User_form()
+    # diction = {'heading': "This is Create for Django Form.", 'new_form': new_form}
+    new_form = forms.Musician()
 
     if request.method == 'POST':
-        new_form = forms.User_form(request.POST)
-        diction.update({'new_form': new_form})
+        # new_form = forms.User_form(request.POST)
+        # diction.update({'new_form': new_form})
+        new_form = forms.Musician(request.POST)
 
         if new_form.is_valid():
+            new_form.save(commit=True)
 
             # user_name = new_form.cleaned_data['User_name']
             # user_dob = new_form.cleaned_data['User_dob']
@@ -26,8 +29,9 @@ def Form(request):
             # diction.update({'User_dob': user_dob})
             # diction.update({'User_email': user_email})
             # diction.update({'field': new_form.cleaned_data['field']})
-            diction.update({'field': "SuccessFull!"})
-            diction.update({'Submitted': 'True'})
+            # diction.update({'field': "SuccessFull!"})
+            # diction.update({'Submitted': 'True'})
 
+    diction = {'heading': "This is Create for Django Form.", 'new_form': new_form}    
     return render(request, "Home/Form.html", context=diction)
     
